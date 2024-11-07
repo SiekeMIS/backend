7624cd4c71076985f9cd8a9f45aef624319e68c1
SiekeMIS <lucasrosas.991402@gmail.com>
2024-11-07 13:19:50 -0300

    Complete el html de listar libros

--- biblioteca/libro/views.py
+++ biblioteca/libro/views.py
@@ -5,17 +5,11 @@ from django.core.paginator import Paginator
 
 def listar_libros(request):
     libros = Libro.objects.select_related('usuario').filter(usuario=request.user)
-    paginator = paginator(libros, 5) #5 libros por paginas
-    page_number = request.GET.get('page')
-    page_onj = paginator.get_page(page_number)
-    return render(request, 'listar_libros.html', {'page_obj':page_onj})
 
-def crear_libros(request):
-    if request.method == 'POST':
-        titulo = request.POST.get('titulo')
-        autor = request.POST.get('autor')
-        genero = request.POST.get('genero')
-        usuario = request.POST.get('usuario')
-        Libro.objects.create(titulo=titulo,autor=autor,genero=genero,usuario=usuario)
-        return redirect('listar_libros')
-    return render(request,'crear_libros.html')
+    paginator = Paginator(libros, 5) #5 libros por paginas
+
+    page_number = request.GET.get('page')
+
+    page_onj = paginator.get_page(page_number)
+
+    return render(request, 'listar_libros.html', {'page_obj':page_onj})
